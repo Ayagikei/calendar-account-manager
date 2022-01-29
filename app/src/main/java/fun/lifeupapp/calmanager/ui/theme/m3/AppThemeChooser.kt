b15519fd188f1f4b096e163fafa6d1eafe2fed7c
AyagiKei <kei.ayagi@gmail.com>
2022-01-29 16:36:30 +0800

    feat: add lifeup blue theme

--- app/src/main/java/fun/lifeupapp/calmanager/ui/theme/m3/AppThemeChooser.kt
+++ app/src/main/java/fun/lifeupapp/calmanager/ui/theme/m3/AppThemeChooser.kt
@@ -1,12 +1,13 @@
 package `fun`.lifeupapp.calmanager.ui.theme.m3
 
+import `fun`.lifeupapp.calmanager.ui.theme.m3.blue.BlueAppTheme
 import `fun`.lifeupapp.calmanager.ui.theme.m3.green.GreenAppTheme
 import `fun`.lifeupapp.calmanager.ui.theme.m3.pink.PinkAppTheme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import kotlin.random.Random
 
-private val isUsingGreenTheme = Random.nextBoolean()
+private val themeRandomInt = Random.nextInt(0, 3)
 
 /**
  * Randomly select an app theme with the app lifecycle.
@@ -16,9 +17,9 @@ fun CalendarManagerM3Theme(
     useDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    if (isUsingGreenTheme) {
-        GreenAppTheme(useDarkTheme, content)
-    } else {
-        PinkAppTheme(useDarkTheme, content)
+    when (themeRandomInt) {
+        0 -> PinkAppTheme(useDarkTheme, content)
+        1 -> GreenAppTheme(useDarkTheme, content)
+        else -> BlueAppTheme(useDarkTheme, content)
     }
 }
